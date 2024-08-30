939184e599befe8c60c3c8f0468c855648389286
Mark Jarjour <mj_git_signing@sent.com>
2024-08-30 18:29:47 -0400

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+import globals from "globals";
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+// @ts-expect-error idk why we need the extension here
+import solid from "eslint-plugin-solid/configs/typescript.js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- likely due to the above
+    solid,
+    eslintConfigPrettier,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+            globals: {
+                ...globals.node,
+            },
+        },
+    }
+);
---
+++ prettier.config.js
@@ -0,0 +1,37 @@
+/** @type {import('prettier').Config} */
+const config = {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    jsxSingleQuote: false,
+    quoteProps: "as-needed",
+    trailingComma: "es5",
+    bracketSpacing: true,
+    arrowParens: "always",
+    endOfLine: "lf",
+    embeddedLanguageFormatting: "auto",
+    overrides: [
+        {
+            files: "*.{md,yml}",
+            options: {
+                tabWidth: 2,
+            },
+        },
+        {
+            files: ".env*",
+            options: {
+                parser: "sh",
+            },
+        },
+    ],
+    // plugins: [
+    //     "prettier-plugin-organize-imports",
+    //     "prettier-plugin-pkg",
+    //     "prettier-plugin-sh",
+    //     "prettier-plugin-tailwindcss", // Tailwind must go last: https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
+    // ],
+};
+
+export default config;
